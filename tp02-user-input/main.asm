8c85ba0b274de21a1a96bfa0d3b95372f717a7a8
elhidraouij <jawad.elhidraoui.pro@gmail.com>
2025-08-20 16:03:52 +0200

    learn to write and read from std output and std input

---
+++ tp02-user-input/main.asm
@@ -0,0 +1,39 @@
+section .data
+  first_msg db 'Please enter a number: '
+  len_first_msg equ $-first_msg
+  second_msg db 'You have entered: '
+  len_second_msg equ $-second_msg
+
+section .bss
+  num resb 5
+
+section .text
+  global _start
+
+_start:
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, first_msg
+  mov edx, len_first_msg
+  int 0x80
+
+  mov eax, 3
+  mov ebx, 2
+  mov ecx, num
+  mov edx, 5
+  int 0x80
+
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, second_msg
+  mov edx, len_second_msg
+  int 0x80
+
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, num
+  mov edx, 5
+  int 0x80
+
+  mov eax, 1
+  int 0x80
